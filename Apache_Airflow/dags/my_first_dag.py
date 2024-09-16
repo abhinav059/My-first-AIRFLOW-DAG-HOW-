e75beb7a2fe8166893c6f6856f90100132675f2c
Abhinav Kumar <abhinavv0179@gmail.com>
2024-09-17 00:40:49 +0530

    DAG example

---
+++ Apache_Airflow/dags/my_first_dag.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 9, 16),  # set a recent start date
+    'retries': 1,
+}
+
+# Function to be executed by PythonOperator
+def simple_hello():
+    print("Hello, Airflow!")
+
+# Define the DAG
+dag = DAG(
+    'simple_hello_world_dag',  # DAG ID
+    default_args=default_args,
+    description='A simple hello world DAG',
+    schedule_interval=None,  # Run manually, not on a schedule
+)
+
+# Define the task using PythonOperator
+hello_task = PythonOperator(
+    task_id='say_hello',  # Task ID
+    python_callable=simple_hello,  # Function to be called
+    dag=dag,  # Pass the DAG instance
+)
+
+# Task dependencies (for more complex DAGs)
+hello_task  # This is a single task DAG, no dependencies
